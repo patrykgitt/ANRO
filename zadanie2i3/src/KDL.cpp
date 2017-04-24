784803fcfd98ffd72e389ef9f6c49b305f77bd67
Piotr Puton <piotr.puton@wp.pl>
2017-04-24 18:00:59 +0200

    Pozdrawiam Pana Kierownika Patryka

--- zadanie2i3/src/KDL.cpp
+++ zadanie2i3/src/KDL.cpp
@@ -11,15 +11,17 @@
 
 double pozycjaZadana[3];
 double const PI=M_PI;
+bool joint_state_aktywny=false;
 
 // Funkcja do pobrania wartości zadanych
 void callback(const sensor_msgs::JointStateConstPtr &msg) 
 {
+	joint_state_aktywny=true;
 	for (int i = 0; i < 3; i++)
 	{
 		pozycjaZadana[i]=msg->position[i];
 	}
-	pozycjaZadana[0]+=PI/5;
+	pozycjaZadana[0]+=PI/5; // Warunek początkowy
 }
 
 int main(int argc, char **argv)
@@ -33,31 +35,74 @@ int main(int argc, char **argv)
 	ros::Rate rate(30);
 
 
-	// Pobranie danych z serwera parametrów
 	double a1=1.0,a2=3.0; // Rzeczywiste wartości do testów
+	double joint2_lower=-2.9,joint2_upper=2.9,joint3_lower=0.3,joint3_upper=3.0; // Dane domyślne 
 	
+	// Pobranie danych z serwera parametrów
+
+	if(!nh.getParam("/arm1",a1))
+		{
+			ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne wartosci.");	
+		}
+	if(!nh.getParam("/arm2",a2))
+		{
+			ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne.wartosci");	
+		}
+	if(!nh.getParam("/joint2_lower",joint2_lower))
+		{
+			ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne wartosci.");	
+		}
+	if(!nh.getParam("/joint2_upper",joint2_upper))
+		{
+					ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne wartosci.");	
+		}
+	if(!nh.getParam("/joint3_lower",joint3_lower))
+		{
+					ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne wartosci.");
+		}
+	if(!nh.getParam("/joint3_upper",joint3_upper))
+		{
+					ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne wartosci.");
+		}
+
 	while(ros::ok())
 	{
-		ros::spinOnce(); // Pobranie informacji od Joint_State_Publisher
+		ros::spinOnce(); // Pobranie informacji od Joint_State_Publishera
+		if(!joint_state_aktywny) // Sprawdzamy, czy już odebraliśmy jakieś dane od joint_state_publishera
+			continue;
+		// Sprawdzanie, czy spełnione są ograniczenia kinematyczne manipulatora
+
+		if(pozycjaZadana[1] > joint2_upper || pozycjaZadana[1] < joint2_lower)
+		{
+			ROS_ERROR("Wyznaczenie polozenia nie jest mozliwe.");
+			continue;
+		}
 		
+		if(pozycjaZadana[2] > joint3_upper || pozycjaZadana[2] < joint3_lower)
+		{
+			ROS_ERROR("Wyznaczenie polozenia nie jest mozliwe.");
+			continue;
+		}
+
 		double x,y,z=0; // Współrzędne końcówki
-		
 		KDL::Chain chain; // Stworzenie łańcucha kinematycznego
 
 		double teta1=pozycjaZadana[0];
 		double teta2 = pozycjaZadana[1];
 		double d3 = pozycjaZadana[2];
+
 		chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ),KDL::Frame::DH(a1, 0, 0, teta1)));
 		chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ),KDL::Frame::DH(a2, PI, 0, teta2)));
 		chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ),KDL::Frame::DH(0, 0, d3, 0)));
 		x = chain.getSegment(2).getFrameToTip().p.data[0];
 		y = chain.getSegment(2).getFrameToTip().p.data[1]; 
-		
+		z = chain.getSegment(2).getFrameToTip().p.data[2];
+
 		geometry_msgs::PoseStamped doWyslania;
 		doWyslania.header.frame_id="arm3";
 		doWyslania.pose.position.x=x;
 		doWyslania.pose.position.y=y+2.0;
-		doWyslania.pose.position.z=z;
+		doWyslania.pose.position.z=0;
 		doWyslania.pose.orientation.w=0.707;
 		doWyslania.pose.orientation.x=0.0;
 		doWyslania.pose.orientation.y=0.0;
--- zadanie2i3/src/NONKDL.cpp
+++ zadanie2i3/src/NONKDL.cpp
@@ -9,15 +9,17 @@
 
 double pozycjaZadana[3];
 double const PI = M_PI;
+bool joint_state_aktywny=false;
 
 // Funkcja do pobrania wartości zadanych
 void callback(const sensor_msgs::JointStateConstPtr &msg) 
 {
+	joint_state_aktywny=true;
 	for (int i = 0; i < 3; i++)
 	{
 		pozycjaZadana[i]=msg->position[i];
 	}
-	pozycjaZadana[0]+=PI/5;
+	pozycjaZadana[0]+=PI/5; // Warunek początkowy
 }
 
 int main(int argc, char **argv)
@@ -33,27 +35,60 @@ int main(int argc, char **argv)
 
 	
 	double a1=1.0,a2=3.0; // Wartości do pobrania z serwera parametrów
-	// Pobranie danych z serwera parametrów
-	//std::vector<double> parametry;
-	std::string parametry;
-	if(nh.getParam("/robot_description",parametry))
-		{
-			std::cout << "mam parametry" << std::endl << parametry;	// W stringu trzeba znaleźć rozmiar 1. arm1 i arm2	
-		}
+	double joint2_lower=-2.9,joint2_upper=2.9,joint3_lower=0.3,joint3_upper=3.0; // Dane domyślne 
 	
+	// Pobranie danych z serwera parametrów
+
+	if(!nh.getParam("/arm1",a1))
+		{
+			ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne wartosci.");	
+		}
+	if(!nh.getParam("/arm2",a2))
+		{
+			ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne.wartosci");	
+		}
+	if(!nh.getParam("/joint2_lower",joint2_lower))
+		{
+			ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne wartosci.");	
+		}
+	if(!nh.getParam("/joint2_upper",joint2_upper))
+		{
+					ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne wartosci.");	
+		}
+	if(!nh.getParam("/joint3_lower",joint3_lower))
+		{
+					ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne wartosci.");
+		}
+	if(!nh.getParam("/joint3_upper",joint3_upper))
+		{
+					ROS_WARN("Nie udalo sie pobrac parametrow. Uzyte zostana domyslne wartosci.");
+		}
+
 	while(ros::ok())
 	{
 		ros::spinOnce(); // Pobranie informacji od Joint_State_Publishera
+		if(!joint_state_aktywny) // Sprawdzamy, czy już odebraliśmy jakieś dane od joint_state_publishera
+			continue;
+		// Sprawdzanie, czy spełnione są ograniczenia kinematyczne manipulatora
+
+		if(pozycjaZadana[1] > joint2_upper || pozycjaZadana[1] < joint2_lower)
+		{
+			ROS_ERROR("Wyznaczenie polozenia nie jest mozliwe.");
+			continue;
+		}
 		
-		
+		if(pozycjaZadana[2] > joint3_upper || pozycjaZadana[2] < joint3_lower)
+		{
+			ROS_ERROR("Wyznaczenie polozenia nie jest mozliwe.");
+			continue;
+		}
 		
 		// Obliczanie parametrów do wysłania
+		
 		double x = a2*cos(pozycjaZadana[0]-pozycjaZadana[1])+a1*cos(pozycjaZadana[0]);
 		double y = a2*sin(pozycjaZadana[0]-pozycjaZadana[1])+a1*sin(pozycjaZadana[0]);
 		double z = -pozycjaZadana[2];
-		// Ewentualnie dodać ograniczenia i logowanie
-
-
+		
 		
 		geometry_msgs::PoseStamped doWyslania;
 		doWyslania.header.frame_id="base_link";
@@ -62,8 +97,6 @@ int main(int argc, char **argv)
 		doWyslania.pose.position.z=z;
 		doWyslania.pose.orientation.w=0;
 		doWyslania.pose.orientation.x=0;
-		
-
 		doWyslania.pose.orientation.y=0;
 		doWyslania.pose.orientation.z=1.0;
 		
